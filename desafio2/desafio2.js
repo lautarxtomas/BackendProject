697d62702cc79efa137e78c14af3847fc7e364cf
lautarxtomas <lautaro.martinez88@mi.unc.edu.ar>
2022-09-20 21:21:29 -0300

    terminando desafio 3

--- desafio2/desafio2.js
+++ desafio2/desafio2.js
@@ -12,7 +12,8 @@ class Container {
         let contObj = JSON.parse(content)
 
         let newId
-        newId = contObj.length > 0 ? contObj.length + 1 : 1;
+        // newId = contObj.length > 0 ? contObj.length + 1 : 1; --> sirve pero no es recomendado.
+        newId = contObj.length ? contObj[contObj.length - 1].id + 1 : 1 // Si hay aunque sea 1 objeto en contObj, busca el último objeto del array y le suma 1 al newId, sino, queda en 1.
         product.id = newId;
 
         contObj.push(product)
@@ -46,8 +47,20 @@ class Container {
 let container = new Container('productos.txt')
 
 // container.save({ "name": "jaggermeister", "price": 10000 })
+// container.save({"name": "fernet", "price": 1500 })
 // container.getById(2)
+// container.deleteById(2)
 // container.deleteById(4)
+// container.deleteById(5)
 // container.deleteAll()
 
-// PROBE TODOS Y ANDAN, NO PUEDO TIRAR MUCHOS SAVE AL MISMO TIEMPO PORQUE SOLO PUSHEA EL ULTIMO.
+// PROBE TODOS Y ANDAN, NO PUEDO TIRAR MUCHOS SAVE AL MISMO TIEMPO PORQUE SOLO PUSHEA EL ULTIMO, E IGUAL CON LOS DELETE
+
+// const usarContenedor = async () => {
+//     await productos.save(producto1)
+//     await productos.save(producto2)
+//     await productos.save(producto3)
+
+// } --------> algo asi tendria que hacer para guardar muchos a la vez
+
+// usarContenedor() 
